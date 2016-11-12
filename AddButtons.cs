986b8cb3f60def7091479b6fdeff4ba7748a21ac
xlera <kazuki.ame@gmail.com>
2016-11-13 02:42:15 +0800

    Add files via upload

---
+++ AddButtons.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddButtons : MonoBehaviour 
+{
+
+	[SerializeField]
+	private Transform puzzleField;
+
+	[SerializeField]
+	private GameObject btn;
+
+
+	void Awake ()
+	{ 
+		for (int i = 0; i < 8; i++)
+		{
+			GameObject button = Instantiate(btn);
+			button.name = "" + i;
+			button.transform.SetParent(puzzleField, false);
+		}
+
+	
+	}
+
+}
